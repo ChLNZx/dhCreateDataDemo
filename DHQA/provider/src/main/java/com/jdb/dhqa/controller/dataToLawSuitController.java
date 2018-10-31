66f46a8a99ce620539bda61ece2f6462bdf7faaa
zx <1078384879@qq.com>
2018-11-01 00:31:52 +0800

    造数据demo提交

---
+++ DHQA/provider/src/main/java/com/jdb/dhqa/controller/dataToLawSuitController.java
@@ -0,0 +1,11 @@
+package com.jdb.dhqa.controller;
+
+/**
+ * Created by xu on 2018/10/31.
+ */
+@Controller
+@RequestMapping()
+public class dataToLawSuitController {
+
+
+}
---
+++ DHQA/provider/src/main/java/com/jdb/dhqa/service/dataToLawSuitService.java
@@ -0,0 +1,7 @@
+package com.jdb.dhqa.service;
+
+/**
+ * Created by xu on 2018/10/31.
+ */
+public class dataToLawSuitService {
+}
